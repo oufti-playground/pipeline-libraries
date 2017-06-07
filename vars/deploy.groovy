e11a9efe409e1cd90be23fad3ff86eaba20b4ba7
Damien Duportal <damien.duportal@gmail.com>
2017-06-07 13:15:28 +0200

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,14 +1,15 @@
 #!/usr/bin/env groovy
 
-def call(String workerImageName) {
+def call(String workerImageName, String registryUrl) {
   stage('Approval') {
     input(message: 'Is it OK to deploy Boss ?', id: 'boss')
   }
   stage('Deploy') {
     node('docker') {
       GIT_SHORT_CHANGESET = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
-      sh "docker tag ${workerImageName} localhost:5000/${workerImageName}:${GIT_SHORT_CHANGESET}"
-      sh "docker push localhost:5000/${workerImageName}:${GIT_SHORT_CHANGESET}"
+      FULL_REGISTRY_IMAGE_NAME="${registryUrl}/${workerImageName}:${GIT_SHORT_CHANGESET}"
+      sh "docker tag ${workerImageName} ${FULL_REGISTRY_IMAGE_NAME}"
+      sh "docker push ${FULL_REGISTRY_IMAGE_NAME}"
     }
   }
 }
