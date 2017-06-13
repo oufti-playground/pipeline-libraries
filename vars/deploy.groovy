cd0fc7753c161640612cc16cf30de9a8765053de
Damien Duportal <damien.duportal@gmail.com>
2017-06-13 15:41:03 +0200

    Create deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -8,8 +8,10 @@ def call(String workerImageName, String registryUrl) {
     node('docker') {
       GIT_SHORT_CHANGESET = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
       FULL_REGISTRY_IMAGE_NAME="${registryUrl}/${workerImageName}:${GIT_SHORT_CHANGESET}"
-      sh "docker tag ${workerImageName}:${GIT_SHORT_CHANGESET} ${FULL_REGISTRY_IMAGE_NAME}"
-      sh "docker push ${FULL_REGISTRY_IMAGE_NAME}"
+      withEnv(['DOCKER_HOST=tcp://docker-service:2375']) {
+        sh "docker tag ${workerImageName}:${GIT_SHORT_CHANGESET} ${FULL_REGISTRY_IMAGE_NAME}"
+        sh "docker push ${FULL_REGISTRY_IMAGE_NAME}"
+      }
     }
   }
 }
